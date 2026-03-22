6dbf940673cdd0a3523594a80c3f06d274ce80d8
Polina <mearrynking@gmail.com>
2026-03-22 20:43:04 +0100

    New game interface

--- WordleDesktop/MainWindow.xaml.cs
+++ WordleDesktop/MainWindow.xaml.cs
@@ -16,10 +16,14 @@ namespace WorldDesktop;
 /// </summary>
 public partial class MainWindow : Window
 {
+    int currentAttempt = 0;
+    string secretWord = "OCEAN";
+
     public MainWindow()
     {
         InitializeComponent();
         CreateGrid();
+        InputBox.Focus();
     }
     
     //Method to create grid for letters
@@ -30,10 +34,10 @@ public partial class MainWindow : Window
         {
             Border card = new Border
             {
-                BorderBrush = new SolidColorBrush(Color.FromRgb(58,58,60)),
+                BorderBrush = new SolidColorBrush(Color.FromRgb(26, 27, 36)),
                 BorderThickness = new Thickness(2),
                 Margin = new Thickness(3),
-                Background = new SolidColorBrush(Color.FromRgb(18,18,19))
+                Background = new SolidColorBrush(Color.FromRgb(237, 239, 250))
             };
 
             card.Child = new TextBlock
@@ -50,6 +54,31 @@ public partial class MainWindow : Window
         }
     }
 
+    //inputting letters to the grid boxes
+    private void InputBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        string text = InputBox.Text.ToUpper();
+        int startIndex = currentAttempt * 5;
+
+        for(int i=0; i<5; i++)
+            {
+                Border card = (Border)GameGrid.Children[startIndex + i];
+                TextBlock txt = (TextBlock)card.Child;
+
+                if(i<text.Length)
+                {
+                    txt.Text = text[i].ToString();
+                    card.BorderBrush = Brushes.Black;
+                    txt.Foreground = Brushes.Black;
+                }
+                else
+                {
+                    txt.Text = "";
+                    card.BorderBrush = new SolidColorBrush(Color.FromRgb(58,58,60));
+                }
+            }
+    }
+
     // Method which works when you click on the button
     private void CheckButton_Click(object sender, RoutedEventArgs e)
     {
@@ -62,6 +91,58 @@ public partial class MainWindow : Window
             return;
         }
 
-        InputBox.Clear();
+        //look at what index raw in grid starts
+        int startIndex = currentAttempt * 5;
+        
+        for(int i=0; i<5; i++)
+        {
+            Border card = (Border)GameGrid.Children[startIndex + i];
+            TextBlock txt = (TextBlock)card.Child;
+
+            txt.Text = guess[i].ToString();
+
+            if(guess[i] == secretWord[i])
+            {
+                card.Background = Brushes.Green;
+                txt.Foreground = Brushes.White;
+            }
+            else if(secretWord.Contains(guess[i]))
+            {
+                card.Background = Brushes.Gold;
+                txt.Foreground = Brushes.Black;
+            }
+            else
+            {
+                card.Background = Brushes.DimGray;
+                txt.Foreground = Brushes.LightGray;
+            }
+        }
+
+        if(guess == secretWord)
+        {
+            MessageBox.Show("Congradulations! You won!");
+            CheckButton.IsEnabled = false;
+        }
+        else
+        {
+            currentAttempt++;
+            InputBox.Clear();
+        }
+    }
+
+    //check button works when user uses enter button
+    private void InputBox_KeyDown(object sender, KeyEventArgs e)
+    {
+        if(e.Key == Key.Enter)
+        {
+            CheckButton_Click(this, new RoutedEventArgs());
+        }
+    }
+
+    //Mehod to protect coursor from going away from the input field
+    protected override void OnPreviewMouseDown(MouseButtonEventArgs e)
+    {
+        base.OnPreviewMouseDown(e);
+        InputBox.Focus();
     }
 }
